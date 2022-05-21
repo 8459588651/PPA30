755c7882c3c1b59288fafddb667f6ddab16f7c3c
Prashant Thombre <98079185+8459588651@users.noreply.github.com>
2022-05-21 08:31:12 +0530

    Add files via upload

---
+++ Dynamic1.c
@@ -0,0 +1,34 @@
+
+#include<stdlib.h>
+
+int main()
+{
+
+	int size = 0;
+	int *ptr = NULL;
+
+
+	printf("Enter number of element :");
+	scanf("%d",&size);
+
+	ptr = malloc(size * sizeof(int));          //step1 : Allocate the memory
+
+
+	if(ptr == NULL)
+	{
+		printf("Unable to allocate thr memory\n");
+
+	}
+	else
+
+	{
+		printf("Memory succsfully allocated");
+	}
+
+	// step 2 :use the memory
+
+
+	free(ptr);     // step 3: free the memory     
+
+	return 0;            
+}
